075a6de52dcd986a310b141e991ef6e81882993e
Ranjit Reddy <ranzit005@gmail.com>
2017-09-15 17:43:40 +0530

    further changes

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 
-//for Angular FireBase Authentication 
+//for Angular FireBase Authentication
 import { AngularFireAuth } from 'angularfire2/auth';
-import firebase from 'firebase/app';
+import { Events, NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { TabsPage } from '../../pages/tabs/tabs';
+import { LoginPage } from '../../pages/login/login';
+import firebase from 'firebase';
 
 @Injectable()
 export class AuthProvider {
 
-constructor(public afAuth: AngularFireAuth) {}
+constructor(public afAuth: AngularFireAuth,public events: Events,public storage: Storage) {}
 
 	loginUser(newEmail: string, newPassword: string): firebase.Promise<any> {
 	  return this.afAuth.auth.signInWithEmailAndPassword(newEmail, newPassword);
@@ -24,5 +28,97 @@ constructor(public afAuth: AngularFireAuth) {}
 	signupUser(newEmail: string, newPassword: string): firebase.Promise<any> {
 	  return this.afAuth.auth.createUserWithEmailAndPassword(newEmail, newPassword);
 	}
+
+}
+//
+
+@Injectable()
+export class UserData {
+  HAS_LOGGED_IN = 'hasLoggedIn';
+  public fireAuth: any;
+
+  constructor(
+    public events: Events,
+    public storage: Storage,
+    public af: AngularFire,
+  ) {
+    this.fireAuth = firebase.auth();
+  }
+
+  login(username: string, password: string): void {
+      this.af.auth.login({ email: username, password: password })
+      .then((auth) => {
+        console.log('Log In Successful, UID: ' + auth.uid + ', ' + 'Email: ' + username);
+        this.storage.set('UID', auth.uid);
+        this.storage.set('EMAIL', username);
+        this.storage.set('PASSWORD', password);
+        this.storage.set(this.HAS_LOGGED_IN, true);
+        this.setUsername(username);
+        this.events.publish('user:login');
+        //this.navCtrl.push(TabsPage);
+      }).catch((err) => {console.log(err.message); this.events.publish('user:login_login.ts', err.message);});
+  };
+
+  signup(username: string, password: string): void {
+    this.af.auth.createUser({ email: username, password: password })
+      .then((auth) => {
+          this.storage.set('UID', auth.uid);
+          this.storage.set('EMAIL', username);
+          this.storage.set('PASSWORD', password);
+          this.storage.set(this.HAS_LOGGED_IN, true);
+          this.setUsername(username);
+          this.events.publish('user:signup');
+      })
+      .catch((err) => {console.log(err.message); this.events.publish('user:signup_signup.ts', err.message);});
+  };
+
+  logout(): void {
+    this.af.auth.logout()
+    .then(() => {
+      this.storage.set('UID', '');
+      this.storage.set('EMAIL', '');
+      this.storage.set('PASSWORD', '');
+      console.log('Log Out Successful');
+      this.storage.remove(this.HAS_LOGGED_IN);
+      this.storage.remove('username');
+      this.events.publish('user:logout');
+      //this.navCtrl.setRoot(LoginPage);
+    })
+    .catch((err) => console.log(err));
+  };
+
+  setUsername(username: string): void {
+    this.storage.set('username', username);
+  };
+
+  getUsername(): Promise<string> {
+    return this.storage.get('username').then((value) => {
+      return value;
+    });
+  };
+
+  getUid(): Promise<string> {
+    return this.storage.get('UID').then((value) => {
+      return value;
+    });
+  };
+
+  hasLoggedIn(): Promise<boolean> {
+    return this.storage.get(this.HAS_LOGGED_IN).then((value) => {
+      return value === true;
+    });
+  };
+
+  resetPassword(email: string): any {
+    return this.fireAuth.sendPasswordResetEmail(email)
+    .then(() => {
+      //console.log('Reset Password Email Sent.');
+      this.events.publish('resetPassword', 'Reset Password Email has been sent Successfully.');
+    })
+    .catch(() => {
+      //console.log('Reset Password Email CANNOT Sent.');
+      this.events.publish('resetPassword', 'Cannot send Reset Password Email for now.');
+    });
+  }
 
 }
